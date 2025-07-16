ff86aebb6e0267d2778b84fba083cb4940055df5
WillWard25 <william.ward@stihl.us>
2025-07-16 15:43:51 -0400

    Add files via upload

---
+++ stihl-pwa/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('pdf-cache').then(cache => {
+      return cache.addAll([
+        'index.html',
+        '2025 PIG.pdf',
+        'manifest.json',
+        'pdfjs/pdf.mjs',
+        'pdfjs/pdf.worker.mjs'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
